6e2443f63192d8ddda0c7df63ee107ec90ab3354
nguyenhuuthang11 <161322267+nguyenhuuthang11@users.noreply.github.com>
2024-11-27 20:37:20 +0700

    Add files via upload

---
+++ schema.sql
@@ -0,0 +1,11 @@
+CREATE DATABASE IF NOT EXISTS usermanagement;
+
+USE usermanagement;
+
+CREATE TABLE IF NOT EXISTS user (
+    userid INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    password VARCHAR(255) NOT NULL,
+    role VARCHAR(255) NOT NULL DEFAULT 'user'
+);
